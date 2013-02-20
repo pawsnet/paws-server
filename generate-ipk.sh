b6fa6b6d077ff8d422eb9fdf5e35d78e8913015c
Arjuna Sathiaseelan <arjuna.sathiaseelan@cl.cam.ac.uk>
2013-02-20 15:43:26 +0000

    changed tp generate-ipk.sh

--- generate-ipk.sh
+++ generate-ipk.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # Generate lastest ipk and upload to Github
 
-version="0.8"
 sdk=/home/paws/OpenWRT-SDK
 pawsrepo=/home/paws/paws-server
 
+
 cd $sdk
-vi package/paws/Makefile
+oldversion="0.8"
+version="0.9"
+sed -i 's/PKG_VERSION:=$oldversion/PKG_VERSION:=$version/g' /package/paws/Makefile
 make V=99
 echo "PAWS ipk $version has been successfully generated"
 
@@ -23,4 +25,7 @@ git pull
 git push
 echo "paws-server Github repo has been updated to reflect this change"
 
-`
+echo "-------------------------------------------"
+echo "The PAWS ipk version: $version is now avalaible from:"
+echo "http://server01.horizon.emnet.co.uk/paws-ipk/paws_$version-1_ar71xx.ipk" 
+echo "and (as a backup) https://github.com/pawsnet/paws-server/blob/master/paws-ipk/paws_$version-1_ar71xx.ipk?raw=true"
