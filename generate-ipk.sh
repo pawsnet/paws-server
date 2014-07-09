0d15a9e1c1973f2c4b31058574f5edaf2b9210a7
paws <paws@univean1.memset.net>
2014-07-09 16:10:54 +0100

    [ipk] fix urls in script pointing to generated ipk

--- generate-ipk.sh
+++ generate-ipk.sh
@@ -18,17 +18,15 @@ make V=99
 
 cp bin/ar71xx/packages/paws_$version-1_ar71xx.ipk  $pawsrepo/paws-ipk
 sudo cp bin/ar71xx/packages/paws_$version-1_ar71xx.ipk  /var/www/html/paws-ipk
-echo "If successful, new ipk is avaliable at http://server01.horizon.emnet.co.uk/paws-ipk/paws_$version-1_ar71xx.ipk"
 
 cd $pawsrepo
+git pull
 git add paws-ipk/paws_$version-1_ar71xx.ipk
 git add Makefile
 git commit -m "adding latest.ipk version $version"
-git pull
-git push
 echo "updating paws-server Github repo has been updated to reflect this change"
+git push
 
 echo "-------------------------------------------"
 echo "The PAWS ipk version: $version is now avalaible from:"
-echo "http://server01.horizon.emnet.co.uk/paws-ipk/paws_$version-1_ar71xx.ipk" 
-echo "and (as a backup) https://github.com/pawsnet/paws-server/blob/master/paws-ipk/paws_$version-1_ar71xx.ipk?raw=true"
+echo "https://github.com/pawsnet/paws-server/blob/master/paws-ipk/paws_$version-1_ar71xx.ipk?raw=true"
